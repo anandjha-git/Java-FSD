9a22af6f9bc471343f1e7fda4e3c223320f54d75
anandjha-git <66514444+anandjha-git@users.noreply.github.com>
2022-08-12 21:31:04 +0530

    Java FSC Phase 2 Practice Project 3 Hibernate

---
+++ Java BackEnd/Hibernet/Add_New_Product/src/main/java/com/company/ProductDAO.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+@WebServlet("/ProductDAO")
+public class ProductDAO extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// reading form values
+				String name = request.getParameter("Pname");
+				int price = Integer.parseInt(request.getParameter("Price"));
+				// put values in Object
+				Product prod = new Product();
+				prod.setPrice(price);
+				prod.setName(name);
+
+				SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+				Session session = sessionFactory.openSession();
+				session.beginTransaction();
+				int i = (Integer) session.save(prod);
+				session.getTransaction().commit();
+
+				session.close();
+
+				PrintWriter out = response.getWriter();
+				if (i > 0)
+					out.println("Record inserted");
+				else
+					out.println("Record not inserted");
+
+			}
+	}
+
+	
+
+
---
+++ Java BackEnd/Hibernet/Add_New_Product/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>To Add New Product , Click On "Add Product" ..</h2>
+<a href="ProductForm.html">Add Product</a>
+</body>
+</html>
